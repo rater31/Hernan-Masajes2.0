87e7803035e79420d92a838c3924449dcb4bedb0
rater31 <fedehidalgo89@gmail.com>
2024-04-03 14:51:30 -0300

    fede

--- src/lib/masajes.ts
+++ src/lib/masajes.ts
@@ -3,6 +3,10 @@ export interface ListaMasaje {
     id:string;
     tipo:string;
     title:string;
+    sub:string;
+    sub2:string;
+    sub3:string;
+    sub4:string;
     contenido:string;
     fondo:string;
 }
@@ -10,8 +14,12 @@ export interface ListaMasaje {
 export const listamasaje: ListaMasaje[] = [
     {
         id:'1',
-        tipo:'Masajes Relajantes',
+        tipo:'Masajes',
         title:'Masajes Relajantes',
+        sub:'masajes californianos',
+        sub2:'masajes Tailandes',
+        sub3:'masajes Lomi Lomi',
+        sub4:'',
         contenido:'askdjhalksdhlahsdlkjasdlkjahsd',
         fondo:'tarjeta1.jpg',
     },
@@ -19,6 +27,10 @@ export const listamasaje: ListaMasaje[] = [
         id:'2',
         tipo:'Masajes ',
         title:'Masajes Descontracturantes',
+        sub:'',
+        sub2:'masajes californianos',
+        sub3:'masajes californianos',
+        sub4:'masajes californianos',
         contenido:'askdjhalksdhlahsdlkjasdlkjahsd',
         fondo:"tarjeta2.jpg",
     }
