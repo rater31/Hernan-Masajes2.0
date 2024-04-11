cdbe3de951c05f129c083b85c8824a10f9ac27f8
rater31 <fedehidalgo89@gmail.com>
2024-04-11 18:52:16 -0300

    fede

---
+++ src/lib/pp.js
@@ -0,0 +1,22 @@
+const btn1 =document.getElementById('btn1');
+const p =document.querySelector('#texto');
+// const btn2 =document.querySelector('#btn2');
+// const p2 =document.querySelector('#texto2');
+// const btn3 =document.querySelector('#btn3');
+// const p3 =document.querySelector('#texto3');
+btn1?.addEventListener("click", ()=>{
+    if  (p?.classList.contains('d-none')) {
+        p.classList.remove('d-none');
+        p.classList.add('d-block');
+        console.log('entro en if')
+    } else if(p?.classList.contains('d-block')) {
+        
+        p?.classList.add('hidden-css'); 
+       p?.classList.remove('d-block');
+       setTimeout(() => {
+        p?.classList.add('d-none');
+        p?.classList.remove('hidden-css');
+       }, 300);
+       console.log('entro en elseif')
+
+    }})
