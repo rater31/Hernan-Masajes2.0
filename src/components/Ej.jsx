cc89f6fe097082ae14ae38e958ce7750afc022c2
rater31 <85116780+rater31@users.noreply.github.com>
2024-04-13 15:59:31 -0300

    fede

--- src/components/Ej.jsx
+++ src/components/Ej.jsx
@@ -3,73 +3,79 @@
 function MyButton(props) {
     // 1
     const handleClick = () => {
-        const p = document.querySelector('#texto');
-        // const botonprueba = document.getElementById('btn1');
-        if (p?.classList.contains('d-none')) {
-            p.classList.remove('d-none');
-            p.classList.add('d-block');
-            console.log('entro en if')
-        } else if (p?.classList.contains('d-block')) {
-            p?.classList.add('hidden-css');
-            p?.classList.remove('d-block');
-            setTimeout(() => {
+        const btn1 =document.getElementById({id});
+        const p =document.getElementById({id});
+        const btn2 =document.querySelector('#btn2');
+        const p2 =document.querySelector('#texto2');
+        const btn3 =document.querySelector('#btn3');
+        const p3 =document.querySelector('#texto3');
+        
+            if  (p?.classList.contains('d-none')) {
+                p.classList.remove('d-none');
+                p.classList.add('d-block');
+                console.log(btn1)
+            } else if(p?.classList.contains('d-block')) {
+                
+                p?.classList.add('hidden-css'); 
+               p?.classList.remove('d-block');
+               setTimeout(() => {
                 p?.classList.add('d-none');
                 p?.classList.remove('hidden-css');
-            }, 300);
-            console.log('entro en elseif')
-
-        }
+               }, 300);
+               console.log('entro en elseif')
+        
+            }
       
     };
-    const handleClick2 =() =>{
-        const p2 = document.querySelector('#texto2');
-        if (p2?.classList.contains('d-none')) {
-            p2.classList.remove('d-none');
-            p2.classList.add('d-block');
-            console.log('entro en if')
-        } else if (p2?.classList.contains('d-block')) {
+    // const handleClick2 =() =>{
+    //     const p2 = document.querySelector('#texto2');
+    //     if (p2?.classList.contains('d-none')) {
+    //         p2.classList.remove('d-none');
+    //         p2.classList.add('d-block');
+    //         console.log('entro en if')
+    //     } else if (p2?.classList.contains('d-block')) {
 
-            p2?.classList.add('hidden-css');
-            p2?.classList.remove('d-block');
-            setTimeout(() => {
-                p2?.classList.add('d-none');
-                p2?.classList.remove('hidden-css');
-            }, 300);
-            console.log('entro en elseif')
+    //         p2?.classList.add('hidden-css');
+    //         p2?.classList.remove('d-block');
+    //         setTimeout(() => {
+    //             p2?.classList.add('d-none');
+    //             p2?.classList.remove('hidden-css');
+    //         }, 300);
+    //         console.log('entro en elseif')
 
-        }
-    }
+    //     }
+    // }
 
-    const handleClick3 =() =>{
-        const p3 = document.querySelector('#texto3');
-        if (p3?.classList.contains('d-none')) {
-            p3.classList.remove('d-none');
-            p3.classList.add('d-block');
-            console.log('entro en if')
-        } else if (p3?.classList.contains('d-block')) {
+    // const handleClick3 =() =>{
+    //     const p3 = document.querySelector('#texto3');
+    //     if (p3?.classList.contains('d-none')) {
+    //         p3.classList.remove('d-none');
+    //         p3.classList.add('d-block');
+    //         console.log('entro en if')
+    //     } else if (p3?.classList.contains('d-block')) {
 
-            p3?.classList.add('hidden-css');
-            p3?.classList.remove('d-block');
-            setTimeout(() => {
-                p3?.classList.add('d-none');
-                p3?.classList.remove('hidden-css');
-            }, 300);
-            console.log('entro en elseif')
+    //         p3?.classList.add('hidden-css');
+    //         p3?.classList.remove('d-block');
+    //         setTimeout(() => {
+    //             p3?.classList.add('d-none');
+    //             p3?.classList.remove('hidden-css');
+    //         }, 300);
+    //         console.log('entro en elseif')
 
-        }
-    }
+    //     }
+    // }
 
     return (
         <div>
-            <div onClick={handleClick}>
-                {props.boton}
+            <div>
+                <button onClick={handleClick}>{props.boton}</button>{props.boton}
                 {props.texto}
             </div>
-            <div onClick={handleClick2}>
+            <div>
                 {props.boton2}
                 {props.texto2}
             </div>
-            <div onClick={handleClick3}>
+            <div>
                 {props.boton3}
                 {props.texto3}
             </div>
