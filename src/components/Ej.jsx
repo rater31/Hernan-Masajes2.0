1e9326bf484df3afa20720998fc0cdd448fa029c
rater31 <fedehidalgo89@gmail.com>
2024-04-15 14:56:27 -0300

    fede

--- src/components/Ej.jsx
+++ src/components/Ej.jsx
@@ -1,86 +1,63 @@
 
 
 function MyButton(props) {
-    // 1
-    const handleClick = () => {
-        const btn1 =document.getElementById({id});
-        const p =document.getElementById({id});
-        const btn2 =document.querySelector('#btn2');
-        const p2 =document.querySelector('#texto2');
-        const btn3 =document.querySelector('#btn3');
-        const p3 =document.querySelector('#texto3');
-        
-            if  (p?.classList.contains('d-none')) {
-                p.classList.remove('d-none');
-                p.classList.add('d-block');
-                console.log(btn1)
-            } else if(p?.classList.contains('d-block')) {
-                
-                p?.classList.add('hidden-css'); 
-               p?.classList.remove('d-block');
-               setTimeout(() => {
-                p?.classList.add('d-none');
-                p?.classList.remove('hidden-css');
-               }, 300);
-               console.log('entro en elseif')
-        
-            }
+
+    // const handleClick = () => {
+    //     const btn1 =document.getElementById('btn11');
+    //     const text1 =document.getElementById('texto11');
+    //         btn1.addEventListener('click',() =>{
+    //             if  (text1?.classList.contains('d-none')) {
+    
+    //                 text1.classList.remove('d-none');
+    //                 text1.classList.add('d-block');
+    
+    //             } else if(text1?.classList.contains('d-block')) {
+                    
+    //                 text1?.classList.add('hidden-css'); 
+    //                text1?.classList.remove('d-block');
+    
+    //                setTimeout(() => {
+    //                 text1?.classList.add('d-none');
+    //                 text1?.classList.remove('hidden-css');
+    //                }, 300);
+    //             }
       
+    // });
+    // // lo que podria hacer seria una function en la cual le asigno el atributo click por separado pero que se ejecute todo junto, eso no creo que sea buena idea   
+    // }
+    const btnClick1 = () =>{
+        const btn1 =  document.getElementById('btn11');
+        const text1 = document.getElementById('texto11');
+        
+        if  (text1?.classList.contains('d-none')&& btn1.id === 'btn11' ) {
+    
+            text1.classList.remove('d-none');
+            text1.classList.add('d-block');
+
+        } else if(text1?.classList.contains('d-block')) {
+            
+            text1?.classList.add('hidden-css'); 
+           text1?.classList.remove('d-block');
+
+           setTimeout(() => {
+            text1?.classList.add('d-none');
+            text1?.classList.remove('hidden-css');
+           }, 300);
+        }
+
+
     };
-    // const handleClick2 =() =>{
-    //     const p2 = document.querySelector('#texto2');
-    //     if (p2?.classList.contains('d-none')) {
-    //         p2.classList.remove('d-none');
-    //         p2.classList.add('d-block');
-    //         console.log('entro en if')
-    //     } else if (p2?.classList.contains('d-block')) {
-
-    //         p2?.classList.add('hidden-css');
-    //         p2?.classList.remove('d-block');
-    //         setTimeout(() => {
-    //             p2?.classList.add('d-none');
-    //             p2?.classList.remove('hidden-css');
-    //         }, 300);
-    //         console.log('entro en elseif')
-
-    //     }
-    // }
-
-    // const handleClick3 =() =>{
-    //     const p3 = document.querySelector('#texto3');
-    //     if (p3?.classList.contains('d-none')) {
-    //         p3.classList.remove('d-none');
-    //         p3.classList.add('d-block');
-    //         console.log('entro en if')
-    //     } else if (p3?.classList.contains('d-block')) {
-
-    //         p3?.classList.add('hidden-css');
-    //         p3?.classList.remove('d-block');
-    //         setTimeout(() => {
-    //             p3?.classList.add('d-none');
-    //             p3?.classList.remove('hidden-css');
-    //         }, 300);
-    //         console.log('entro en elseif')
-
-    //     }
-    // }
 
     return (
-        <div>
-            <div>
-                <button onClick={handleClick}>{props.boton}</button>{props.boton}
-                {props.texto}
-            </div>
-            <div>
-                {props.boton2}
-                {props.texto2}
-            </div>
-            <div>
-                {props.boton3}
-                {props.texto3}
-            </div>
 
-        </div>
+                <>
+                
+                <span onClick={btnClick1} >{props.boton}</span>
+                
+                {props.texto}
+  
+                </>
+
     );
 }
 
