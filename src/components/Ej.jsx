0268950e20ca49b41929b5464efe7e76d2fb8401
rater31 <85116780+rater31@users.noreply.github.com>
2024-04-15 19:49:33 -0300

    fede

--- src/components/Ej.jsx
+++ src/components/Ej.jsx
@@ -2,50 +2,29 @@
 
 function MyButton(props) {
 
-    // const handleClick = () => {
-    //     const btn1 =document.getElementById('btn11');
-    //     const text1 =document.getElementById('texto11');
-    //         btn1.addEventListener('click',() =>{
-    //             if  (text1?.classList.contains('d-none')) {
-    
-    //                 text1.classList.remove('d-none');
-    //                 text1.classList.add('d-block');
-    
-    //             } else if(text1?.classList.contains('d-block')) {
-                    
-    //                 text1?.classList.add('hidden-css'); 
-    //                text1?.classList.remove('d-block');
-    
-    //                setTimeout(() => {
-    //                 text1?.classList.add('d-none');
-    //                 text1?.classList.remove('hidden-css');
-    //                }, 300);
-    //             }
-      
-    // });
-    // // lo que podria hacer seria una function en la cual le asigno el atributo click por separado pero que se ejecute todo junto, eso no creo que sea buena idea   
-    // }
-    const btnClick1 = () =>{
-        const btn1 =  document.getElementById('btn11');
-        const text1 = document.getElementById('texto11');
+    const btnClick1 = (e) =>{
         
-        if  (text1?.classList.contains('d-none')&& btn1.id === 'btn11' ) {
+        const btn =  document.getElementById('btn11');
+        const text1 = document.getElementById('texto11');
+        console.log(e.target.id)
+
+        // console.log(id)
+        // if  (text1?.classList.contains('d-none') ) {
     
-            text1.classList.remove('d-none');
-            text1.classList.add('d-block');
+        //     text1.classList.remove('d-none');
+        //     text1.classList.add('d-block');
 
-        } else if(text1?.classList.contains('d-block')) {
+        // } else if(text1?.classList.contains('d-block')) {
             
-            text1?.classList.add('hidden-css'); 
-           text1?.classList.remove('d-block');
+        //     text1?.classList.add('hidden-css'); 
+        //    text1?.classList.remove('d-block');
 
-           setTimeout(() => {
-            text1?.classList.add('d-none');
-            text1?.classList.remove('hidden-css');
-           }, 300);
-        }
-
-
+        //    setTimeout(() => {
+        //     text1?.classList.add('d-none');
+        //     text1?.classList.remove('hidden-css');
+        //    }, 300);
+        // }
+    
     };
 
     return (
